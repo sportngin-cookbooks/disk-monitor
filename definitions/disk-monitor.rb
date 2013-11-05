3ac3e48c63e3508fff61f1e3134d14d303622596
Andy Fleener <andrew.fleener@sportngin.com>
2013-11-05 14:34:28 -0600

    Revert "Fix defaults"
    This reverts commit a0648dfd8eca335f0503549d463d8506dad76be3.

--- definitions/disk-monitor.rb
+++ definitions/disk-monitor.rb
@@ -1,16 +1,16 @@
-define :disk_monitor do
+define :disk_monitor, :alerting_threshold => 90, :template => "alert.sh.erb", :bin_path => "/usr/local/bin/disk-alert" do
   application = Hash.new("").tap do |a|
     a[:app_name] = params[:app_name]
     a[:environment] = params[:environment]
     a[:pd_service_key] = params[:pd_service_key]
     a[:hostname] = params[:hostname] || node[:hostname]
-    a[:alerting_threshold] = params[:alerting_threshold] || 90
+    a[:alerting_threshold] = params[:alerting_threshold].to_i
     a[:user] = params[:user] || "root"
     a[:group] = params[:group] || "root"
-    a[:template] = params[:template] ||"alert.sh.erb"
+    a[:template] = params[:template]
     a[:cookbook] = params[:cookbook] || "disk-monitor"
     a[:check_frequency] = params[:check_frequency] || 15
-    a[:bin_path] = params[:bin_path] || "/usr/local/bin/disk-alert"
+    a[:bin_path] = params[:bin_path]
   end
 
   template a[:bin_path] do
