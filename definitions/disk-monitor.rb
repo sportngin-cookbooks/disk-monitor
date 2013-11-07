15d13ae6dd1c0a1b07e79b0bda35a843603374c3
Andy Fleener <andrew.fleener@sportngin.com>
2013-11-07 12:50:25 -0600

    Allow check frequency to be every minute

--- definitions/disk-monitor.rb
+++ definitions/disk-monitor.rb
@@ -11,6 +11,7 @@ define :disk_monitor, :template => "alert.sh.erb", :bin_path => "/usr/local/bin"
     a[:template] = params[:template]
     a[:cookbook] = params[:cookbook] || "disk-monitor"
     a[:check_frequency] = params[:check_frequency] || 15
+    a[:cron_frequency] = a[:check_frequency] == 1 ? "*" : "*/#{a[:check_frequency]}"
     a[:bin_path] = params[:bin_path]
     a[:bin_file] = params[:bin_file] || "disk-alert-#{a[:alert_level]}"
     a[:bin] = "#{a[:bin_path]}/#{a[:bin_file]}"
@@ -26,7 +27,7 @@ define :disk_monitor, :template => "alert.sh.erb", :bin_path => "/usr/local/bin"
   end
 
   cron "disk-alert-#{application[:alert_level]}" do
-    minute "*/#{application[:check_frequency]}"
+    minute a[:cron_frequency]
     user application[:user]
     command application[:bin]
   end
